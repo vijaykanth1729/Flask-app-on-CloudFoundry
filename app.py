b8e99867a69b5d4be08d29108dc5b5029bcb0260
vijaykanth1729 <personalvijay007@gmail.com>
2021-01-06 18:04:41 +0530

    updated with /admin route

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def home():
 @app.route('/test')
 def test():
     return "<h2>Testing this endpoint, for CloudFoundry Automatic Push :)"
-
+@app.route('/admin')
+def admin_login():
+    return "<h3>You are not authorized to perform login activity, Contact your Administrator for more help!!!</h3>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
